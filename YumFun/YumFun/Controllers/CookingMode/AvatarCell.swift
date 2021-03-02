3c76232511691dd8c8d493b357b449e0c7bec93e
ChrissyKX <47626703+ChrissyKX@users.noreply.github.com>
2021-03-03 02:08:15 +0800

    prepare view basic set up, for merge purpose, not fully functioning

---
+++ YumFun/YumFun/Controllers/CookingMode/AvatarCell.swift
@@ -0,0 +1,13 @@
+//
+//  AvatarCellCollectionViewCell.swift
+//  YumFun
+//
+//  Created by Xiyu Zhang on 3/2/21.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    @IBOutlet weak var avatar: CircularImageView!
+    
+}
