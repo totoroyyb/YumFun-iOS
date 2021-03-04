e869767d40d3ce66029c47eedc9fd755d1995524
ChrissyKX <47626703+ChrissyKX@users.noreply.github.com>
2021-03-05 01:53:23 +0800

    for branch switching purpose

---
+++ YumFun/YumFun/Controllers/CookingMode/CookingViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CookingViewController.swift
+//  YumFun
+//
+//  Created by Xiyu Zhang on 3/4/21.
+//
+
+import UIKit
+
+class CookingViewController: UIViewController {
+
+    @IBOutlet weak var stepCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
