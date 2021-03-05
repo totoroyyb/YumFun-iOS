da9620ae31eeb355125f570d4cf4d0e8e9295812
Ruifeng Zhang <failury29@gmail.com>
2021-03-05 13:51:54 -0800

    remove search

--- YumFun/YumFun/Controllers/Profile/UserListViewController.swift
+++ YumFun/YumFun/Controllers/Profile/UserListViewController.swift
@@ -9,8 +9,6 @@ import UIKit
 
 class UserListViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     var List:[String] = []
-    var SearchedList:[String] = []
-    var searching = false
     
     @IBOutlet weak var Searchbar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
@@ -23,24 +21,15 @@ class UserListViewController: UIViewController,UITableViewDelegate,UITableViewDa
             self.tableView.backgroundView = emptyLabel
             self.tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
             return 0
-        } else if searching == false {
+        } else {
             return self.List.count
         }
-        else if searching == true{
-            return self.SearchedList.count
-        }
-        return 0
+    
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var DisplayList: [String]
-        if searching {
-            DisplayList = SearchedList
-        }else{
-            DisplayList = List
-        }
         let cell = tableView.dequeueReusableCell(withIdentifier:"usercell",for: indexPath) as! UserCell
-        User.get(named: DisplayList[indexPath.row],{_,user,_ in
+        User.get(named: List[indexPath.row],{_,user,_ in
             guard let u = user else{return}
             cell.UserName.text = u.displayName
             cell.UserBio.text = u.bio
@@ -55,18 +44,7 @@ class UserListViewController: UIViewController,UITableViewDelegate,UITableViewDa
         return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            if searching {
-                User.get(named: SearchedList[indexPath.row]) { (_,User, _) in
-                    guard let U = User else {return}
-                    guard let UserView = self.storyboard?.instantiateViewController(withIdentifier: "ProfileView") as? ProfileViewController else {
-                        assertionFailure("Cannot find ViewController")
-                        return
-                    }
-                    UserView.OU = U
-                    UserView.isSelf = false
-                    self.navigationController?.pushViewController(UserView, animated: true)
-                }
-            } else {
+         
                 
                 User.get(named: List[indexPath.row]) { (_,User, _) in
                     guard let U = User else {return}
@@ -78,9 +56,6 @@ class UserListViewController: UIViewController,UITableViewDelegate,UITableViewDa
                     UserView.isSelf = false
                     self.navigationController?.pushViewController(UserView, animated: true)
                 }
-            }
-            // Close keyboard when you select cell
-        self.Searchbar.searchTextField.endEditing(true)
         }
     
     
@@ -100,16 +75,3 @@ class UserListViewController: UIViewController,UITableViewDelegate,UITableViewDa
     
 }
 
-extension UserListViewController: UISearchBarDelegate {
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        SearchedList = List.filter { $0.lowercased().prefix(searchText.count) == searchText.lowercased() }
-                searching = true
-                tableView.reloadData()
-    }
-    
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        searching = false
-                searchBar.text = ""
-                tableView.reloadData()
-    }
-}
